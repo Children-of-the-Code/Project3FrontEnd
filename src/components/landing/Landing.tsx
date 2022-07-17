bb2daf7184acdaf45cdad312f618e80ae4a1faf0
Kris Paschal <krispaschal@gmail.com>
2022-07-17 16:43:12 -0500

    updated landing style

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,48 +1,34 @@
 import * as React from 'react';
-import styled from "styled-components";
-import Colonel from '../../assets/colonel.png';
+import Market from '../../assets/market.jpg';
 
-const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-`;
-
-const Card = styled.div`
-  text-align: center;
-`
-const CardTitle = styled.h1`
-  font-size: 2em;
-  text-align: center;
-  color: #8CC63E;
-`;
-
-const CardSubtitle = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #DD6031;
-`;
-
-const CardText = styled.h1`
-  font-size: 1.25em;
-  text-align: justify;
-  color: #000000;
-`;
 
 export default function Landing() {
   return (
-    <>
-      <Container>
-        <Card>
-          <CardTitle>Colonel Kernel's</CardTitle>
-          <img src={Colonel} alt="Logo" height="120px"/>
-          <CardSubtitle>Farmers Market</CardSubtitle>
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis consequuntur possimus repellat facilis assumenda accusantium, libero minus quos ex facere fugiat ut, necessitatibus ea impedit natus eveniet earum doloremque? Laborum quibusdam consequatur, soluta dolor deserunt tempore eos maiores perferendis animi commodi? Neque inventore, assumenda laudantium vel quo iure ipsum reiciendis?
-          </CardText>
-        </Card>
-      </Container>
-    </>
+    <div className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight text-green-500 font-extrabold sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Precious Produce</span>
+                <br />
+                <span className="block text-yellow-400 xl:inline">To enhance your life</span>
+              </h1>
+              <p className="mt-3 text-base text-orange-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+                fugiat veniam occaecat fugiat aliqua.
+              </p>
+            </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src={Market}
+          alt=""
+        />
+      </div>
+    </div>
   );
 };
