b36ebf679044042bbc8669b9e6765ac04e01af2e
Kris Paschal <krispaschal@gmail.com>
2022-07-17 17:11:29 -0500

    updated cart ui

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,137 +1,11 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import { CartContext } from "../../context/cart.context";
-const Container = styled.div``;
 
-const Wrapper = styled.div`
-  padding: 20px;
-`;
 
-const Title = styled.h1`
-  font-weight: 600;
-  text-align: center;
-  color: #8CC63E;
-  
-`;
 
-const Top = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px;
-`;
 
-const TopButton = styled.button`
-  padding: 10px;
-  font-weight: 600;
-  cursor: pointer;
-  background-color: #8CC63E;
-  color: #fac748;
-`;
 
-const Bottom = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Info = styled.div`
-  flex: 3;
-`;
-
-const Product = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ProductDetail = styled.div`
-  flex: 2;
-  display: flex;
-`;
-
-const Image = styled.img`
-  width: 200px;
-`;
-
-const Details = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-
-const ProductName = styled.span``;
-
-const ProductId = styled.span``;
-
-const ProductColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-`;
-
-const ProductSize = styled.span``;
-
-const PriceDetail = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const ProductAmountContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-
-const ProductAmount = styled.div`
-  font-size: 24px;
-  margin: 5px;
-`;
-
-const ProductPrice = styled.div`
-  font-size: 30px;
-  font-weight: 400;
-`;
-
-const Hr = styled.hr`
-  background-color: #eee;
-  border: none;
-  height: 1px;
-`;
-
-const Summary = styled.div`
-  flex: 1;
-  border: 0.5px solid lightgray;
-  border-radius: 10px;
-  padding: 20px;
-  height: 50vh;
-`;
-
-const SummaryTitle = styled.h1`
-  font-weight: 200;
-  color: '#8CC63E'
-`;
-
-const SummaryItem = styled.div`
-  margin: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const SummaryItemText = styled.span``;
-
-const SummaryItemPrice = styled.span``;
-
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #8CC63E;
-  color: #fac748;
-  font-weight: 600;
-`;
 
 export const Cart = () => {
   const { cart, setCart } = useContext(CartContext);
@@ -139,71 +13,78 @@ export const Cart = () => {
   const navigate = useNavigate();
 
   return (
-    <Container>
-  
-      <Wrapper>
-        <Title>YOUR RUCK!</Title>
-        <Top>
-          <TopButton onClick={() => {navigate('/products')}}>CONTINUE SHOPPING</TopButton>
-        </Top>
-        <Bottom>
-          <Info>
-            {
+    <div className="container mx-auto mt-10">
+    <div className="flex shadow-md my-10">
+      <div className="w-3/4 bg-white px-10 py-10">
+        <div className="flex justify-between border-b pb-8">
+          <h1 className="font-semibold text-2xl">Shopping Cart</h1>
+        </div>
+        {/* Cart Column Header */}
+        <div className="flex mt-10 mb-5">
+          <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
+          <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
+          <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
+          <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+        </div>
+        {/* Cart Map Start */}
+        {
               cart.map((product)=> (
                 <>
-                  <Product>
-                    <ProductDetail>
-                      <Image src={product.image} />
-                      <Details>
-                        <ProductName>
-                          <b>Product:</b> {product.name}
-                        </ProductName>
-                        <ProductId>
-                          <b>Description:</b> {product.description}
-                        </ProductId>
-                      </Details>
-                    </ProductDetail>
-                    <PriceDetail>
-                      <ProductAmountContainer>
-                        <ProductAmount> 
-                          <b>You are ordering: </b>
-                          {product.quantity} 
-                        </ProductAmount>
-                      </ProductAmountContainer>
-                      <ProductPrice>$ {product.price}.00 .ea</ProductPrice>
-                    </PriceDetail>
-                  </Product>
-                  <Hr/>
-                </>
+        <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+          <div className="flex w-2/5"> 
+            <div className="w-20">
+              <img className="h-24" src={product.image} alt="" />
+            </div>
+            <div className="flex flex-col justify-between ml-4 flex-grow">
+              <span className="font-bold text-sm">{product.name}</span>
+              <span className="text-red-500 text-xs">{product.description}</span>
+              <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
+            </div>
+          </div>
+          <div className="flex justify-center w-1/5">
+            <input className="mx-2 border text-center w-8" type="text" value={product.quantity} />
+          </div>
+          <span className="text-center w-1/5 font-semibold text-sm">${product.price}</span>
+          <span className="text-center w-1/5 font-semibold text-sm">${product.price * product.quantity}</span>
+        </div>
+        </>
               ))
             }
-          </Info>
-          <Summary>
-            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
-            <SummaryItem>
-              <SummaryItemText>Subtotal</SummaryItemText>
-              <SummaryItemPrice>$ 
-                  {cart.reduce<number>((total, product) => total + product.price * product.quantity, 0)}.00
-              </SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Estimated Shipping</SummaryItemText>
-              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Shipping Discount</SummaryItemText>
-              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Total</SummaryItemText>
-              <SummaryItemPrice>$ 
-                {cart.reduce<number>((total, product) => total + product.price * product.quantity, 0)}.00
-              </SummaryItemPrice>
-            </SummaryItem>
-            <Button onClick={() => {navigate('/checkout')}}>CHECKOUT NOW!</Button>
-          </Summary>
-        </Bottom>
-      </Wrapper>
-    </Container>
-  );
+      {/* Cart Map End */}
+      {/* Link to Shop */}
+        <a href="#" className="flex font-semibold text-orange-600 text-sm mt-10">
+      
+          <svg className="fill-current mr-2 text-orange-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
+          Continue Shopping
+        </a>
+      </div>
+      {/* Order Summary Start */}
+      <div id="summary" className="w-1/4 px-8 py-10">
+        <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+        <div className="flex justify-between mt-10 mb-5">
+          <span className="font-semibold text-sm uppercase">Items {cart.reduce<number>((total, product) => total + product.quantity, 0)}</span>
+          <span className="font-semibold text-sm">${cart.reduce<number>((total, product) => total + product.price * product.quantity, 0)}</span>
+        </div>
+        <div>
+          <div className="flex justify-between mt-10 mb-5">
+          <span className="font-semibold text-sm uppercase">Standard Shipping</span>
+          <span className="font-semibold text-sm">$4.95</span>
+        </div>
+          <div className="flex justify-between mt-10 mb-5">
+          <span className="font-semibold text-sm uppercase">Shipping Discount</span>
+          <span className="font-semibold text-sm">-$4.95</span>
+        </div>
+        </div>
+        <div className="border-t mt-8">
+          <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+            <span>Total cost</span>
+            <span>${cart.reduce<number>((total, product) => total + product.price * product.quantity, 0)}</span>
+          </div>
+          <button onClick={() => {navigate('/checkout')}} className="bg-green-500 font-semibold hover:bg-green-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  )
 };
