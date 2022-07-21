10438f71c77d9fad2839851537fa895822e7b4a7
Fourpointstar <81208827+Fourpointstar@users.noreply.github.com>
2022-07-20 23:52:53 -0400

    Formatting for the user view of products

--- src/components/display-products/ProductCard.tsx
+++ src/components/display-products/ProductCard.tsx
@@ -47,7 +47,15 @@ export const ProductCard = (props: productProps) => {
     <h3 className="mt-4 text-lg font-medium text-gray-700">{props.product.name}</h3>
     
     <h3 className="mt-4 text-md font-medium text-gray-700">{props.product.description}</h3>
-    <p className="mt-1 text-lg font-medium text-gray-900 mb-4">${props.product.price.toFixed(2)}</p>
+    {props.product.sale>0&&<>
+    <h3 className="mt-4 text-md font-medium text-gray-700">Old Price: $<s>{((props.product.price)/((100-props.product.sale)/100)).toFixed(2)}</s></h3>
+    <h3 className="mt-4 text-md font-medium text-gray-700">Sale: %{props.product.sale}</h3>
+    <h3 className="mt-4 text-md font-medium text-gray-700">New Price: ${(props.product.price).toFixed(2)}</h3>
+    </>
+    }
+    {props.product.sale===0&&<>
+    <p className="mt-1 text-lg font-medium text-gray-900">Price: ${(props.product.price).toFixed(2)}</p>
+    </>}
    
     {/* User UI */}
     <div className="px-2 py-2 text-center sm:px-2 mx-2">
